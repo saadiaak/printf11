57d84f71097b679e0e67256538cba97e56b19d1c
SAADIA <122810943+saadiaak@users.noreply.github.com>
2023-03-26 22:18:59 +0000

    Update main.h

--- main.h
+++ main.h
@@ -1,212 +1,89 @@
-#ifndef MAIN_H
+Skip to content
+Search or jump to…
+Pull requests
+Issues
+Codespaces
+Marketplace
+Explore
+ 
+@saadiaak 
+BrightOlawale
+/
+printf
+Public
+Fork your own copy of BrightOlawale/printf
+Code
+Issues
+Pull requests
+Actions
+Projects
+Security
+Insights
+printf/main.h
+@BrightOlawale
+BrightOlawale case S dealt with
+Latest commit 9703f9f on Apr 22, 2022
+ History
+ 2 contributors
+@BrightOlawale@cynthiaclaire4
+44 lines (39 sloc)  1002 Bytes
 
-#define MAIN_H
-
-#include <stdarg.h>
-
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 #include <unistd.h>
-
-
-#define UNUSED(x) (void)(x)
-
-#define BUFF_SIZE 1024
-
-
-/* FLAGS */
-
-#define F_MINUS 1
-
-#define F_PLUS 2
-
-#define F_ZERO 4
-
-#define F_HASH 8
-
-#define F_SPACE 16
-
-
-/* SIZES */
-
-#define S_LONG 2
-
-#define S_SHORT 1
-
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
-
- * struct fmt - Struct op
-
+ * struct symfun - defines a structure for the specifier and functions
  *
-
- * @fmt: The format.
-
- * @fn: The function associated.
-
+ * @symb: The operator
+ * @f: The function associated
  */
-
-struct fmt
-
+struct symfun
 {
-
-        char fmt;
-
-        int (*fn)(va_list, char[], int, int, int, int);
-
+	char *symb;
+	int (*f)(va_list);
 };
+typedef struct symfun symbfunc;
 
-
-
-/**
-
- * typedef struct fmt fmt_t - Struct op
-
- *
-
- * @fmt: The format.
-
- * @fm_t: The function associated.
-
- */
-
-typedef struct fmt fmt_t;
-
-
+/*Main fuctions*/
+int compute(const char *format, symbfunc options[], va_list arg_param);
 int _printf(const char *format, ...);
-
-int handle_print(const char *fmt, int *i,
-
-va_list list, char buffer[], int flags, int width, int precision, int size);
-
-
-/****************** FUNCTIONS ******************/
-
-
-/* Funtions to print chars and strings */
-
-int print_char(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_string(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_percent(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/* Functions to print numbers */
-
-int print_int(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_binary(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_unsigned(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_octal(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_hexadecimal(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int print_hexa_upper(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-int print_hexa(va_list types, char map_to[],
-
-char buffer[], int flags, char flag_ch, int width, int precision, int size);
-
-
-/* Function to print non printable characters */
-
-int print_non_printable(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/* Funcion to print memory address */
-
-int print_pointer(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/* Funciotns to handle other specifiers */
-
-int get_flags(const char *format, int *i);
-
-int get_width(const char *format, int *i, va_list list);
-
-int get_precision(const char *format, int *i, va_list list);
-
-int get_size(const char *format, int *i);
-
-
-/*Function to print string in reverse*/
-
-int print_reverse(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/*Function to print a string in rot 13*/
-
-int print_rot13string(va_list types, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/* width handler */
-
-int handle_write_char(char c, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int write_number(int is_positive, int ind, char buffer[],
-
-        int flags, int width, int precision, int size);
-
-int write_num(int ind, char bff[], int flags, int width, int precision,
-
-        int length, char padd, char extra_c);
-
-int write_pointer(char buffer[], int ind, int length,
-
-        int width, int flags, char padd, char extra_c, int padd_start);
-
-
-int write_unsgnd(int is_negative, int ind,
-
-char buffer[],
-
-        int flags, int width, int precision, int size);
-
-
-/****************** UTILS ******************/
-
-int is_printable(char);
-
-int append_hexa_code(char, char[], int);
-
-int is_digit(char);
-
-
-long int convert_size_number(long int num, int size);
-
-long int convert_size_unsgnd(unsigned long int num, int size);
-
-
-#endif /* MAIN_H */
+int _writechar(char);
+int print_char(va_list);
+int print_string(va_list);
+int print_number(va_list args_list);
+int print_integer(va_list arg_list);
+int print_binary(va_list);
+int unsigned_integer(va_list);
+int print_octal(va_list);
+int print_HeX(va_list);
+int print_hex(va_list);
+int print_np_string(va_list);
+
+/* additional functions */
+unsigned int base_len(unsigned int, int);
+void _writebase(char *str);
+char *rev_string(char *s);
+char *_memcpy(char *dest, char *scr, unsigned int n);
+int print_unsgined_number(unsigned int);
+
+#endif
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+printf/main.h at master · BrightOlawale/printf
